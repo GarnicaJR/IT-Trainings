f62793fb361ad50d1ad923a911b1a6a328de9b8a
GarnicaJR <jose.garnica.lomeli@gmail.com>
2020-03-09 20:33:29 +0000

    codebase

---
+++ it-trainings/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ThemeSwitcher from './components/ThemeSwitcher';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    <ThemeSwitcher></ThemeSwitcher>
+    </div>
+  );
+}
+
+export default App;
